163086153e419573beadbd359a9cc739dd1ba44a
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-22 13:22:32 +0000

    Implementing ingestion protocol

---
+++ ingestion/ingest.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from config.settings import get_settings
+
+settings = get_settings()
+
+def ingest(docs_path: str = settings.raw_documents_dir):
+    loader = DirectoryLoader(docs_path, loader_cls=TextLoader)
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size = settings.chunk_size,
+                                              chunk_overlap= settings.chunk_overlap)
+    
+    chunks = splitter.split_documents(docs)
+
+    Chroma.from_documents(
+        documents = chunks,
+        embedding = OllamaEmbeddings(model = settings.embedding_model),
+        collection_name = settings.collection_name,
+        persist_directory = settings.chroma_persist_dir 
+    )
+
+    print(f'Indexados {len(chunks)} chunks de {len(docs)} documentos')
+
+if __name__ == "__main__":
+    ingest()
