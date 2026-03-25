7f35560cec64970091531490a04d3ec609c2e7ed
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-25 23:06:38 +0000

    Refactoring de ragas module as a class

--- evaluation/evaluators.py
+++ evaluation/evaluators.py
@@ -1,11 +1,4 @@
 from ragas import evaluate 
-from ragas.metrics import (
-    faithfulness, 
-    answer_relevancy,
-    context_precision,
-    answer_correctness
-)
-
 from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
 from langchain_ollama import ChatOllama, OllamaEmbeddings
@@ -14,56 +7,41 @@ from config.settings import get_settings
 
 settings = get_settings()
 
-def get_ragas_config():
-    """
+class RagasEvaluator():
+
+    def __init__(self, ragas_dataset, experiment_name):
+        self.ragas_dataset = ragas_dataset
+        self.experiment_name = experiment_name
+        self.ragas_llm = settings.llm_model
+        self.ragas_embedded = settings.embedding_model
+        self.metrics = settings.metrics
+
+    def run_evaluation(self) -> dict:
+        """
     
-    """
+        """
 
-    llm = LangchainLLMWrapper(
-        ChatOllama(model = settings.llm_model, temperature = 0)
-    )
+        for metric in self.metrics:
+            metric.llm = self.ragas_llm
 
-    embeddings = LangchainEmbeddingsWrapper(
-        OllamaEmbeddings(model = settings.embedding_model)
-    )
+            if hasattr(metric, "embeddings"):
+                metric.embeddings = self.ragas_embedded
 
-    return llm, embeddings
+        print(f"Executing validation '{self.experiment_name}'...")
+        print(f"Examples: {len(self.ragas_dataset)}")
+        print(f"Metrics: {[m.name for m in self.metrics]}")
 
-def run_evaluation(ragas_dataset, experiment_name: str = "eval-v1") -> dict:
-    """
+        results = evaluate(
+            dataset = self.ragas_dataset,
+            metrics = self.metrics,
+        )
+
+        scores = results.to_pandas()
+        print("\n=== Results ===")
     
-    """
-
-    llm, embeddings = get_ragas_config()
-
-    metrics = [
-        faithfulness,
-        answer_relevancy,
-        context_precision,
-        answer_correctness
-    ]
-
-    for metric in metrics:
-        metric.llm = llm
-
-        if hasattr(metric, "embeddings"):
-            metric.embeddings = embeddings
-
-    print(f"Executing validation '{experiment_name}'...")
-    print(f"Examples: {len(ragas_dataset)}")
-    print(f"Metrics: {[m.name for m in metrics]}")
-
-    results = evaluate(
-        dataset = ragas_dataset,
-        metrics = metrics,
-    )
-
-    scores = results.to_pandas()
-    print("\n=== Results ===")
+        for metric in ["faithfulness", "answer_relevancy", "context_precision", "answer_correctness"]:
+            if metric in scores.columns:
+                mean = scores[metric].mean()
+                print(f" {metric}: {mean: .3f}")
     
-    for metric in ["faithfulness", "answer_relevancy", "context_precision", "answer_correctness"]:
-        if metric in scores.columns:
-            mean = scores[metric].mean()
-            print(f" {metric}: {mean: .3f}")
-    
-    return results
+        return results
