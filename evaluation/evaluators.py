25bf77b6b9355420739243f690ec37e47fac040b
Pablo Guilló <pabloguilloj@gmail.com>
2026-03-25 22:22:42 +0000

    Function that runs the evaluation/run_evaluation

--- evaluation/evaluators.py
+++ evaluation/evaluators.py
@@ -28,3 +28,42 @@ def get_ragas_config():
     )
 
     return llm, embeddings
+
+def run_evaluation(ragas_dataset, experiment_name: str = "eval-v1") -> dict:
+    """
+    
+    """
+
+    llm, embeddings = get_ragas_config()
+
+    metrics = [
+        faithfulness,
+        answer_relevancy,
+        context_precision,
+        answer_correctness
+    ]
+
+    for metric in metrics:
+        metric.llm = llm
+
+        if hasattr(metric, "embeddings"):
+            metric.embeddings = embeddings
+
+    print(f"Executing validation '{experiment_name}'...")
+    print(f"Examples: {len(ragas_dataset)}")
+    print(f"Metrics: {[m.name for m in metrics]}")
+
+    results = evaluate(
+        dataset = ragas_dataset,
+        metrics = metrics,
+    )
+
+    scores = results.to_pandas()
+    print("\n=== Results ===")
+    
+    for metric in ["faithfulness", "answer_relevancy", "context_precision", "answer_correctness"]:
+        if metric in scores.columns:
+            mean = scores[metric].mean()
+            print(f" {metric}: {mean: .3f}")
+    
+    return results
